6a3656bd14bfd06b9f933ca66558df7cc3bda4bf
Roy Palomino <roy100995@gmail.com>
2021-06-15 21:14:20 -0500

    refactor: changed color themes and added a navigation

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -5,6 +5,51 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.white),
+        title: Text(
+          'AGENDAR SERVICIO TÉCNICO',
+          style: TextStyle(color: Colors.white),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.account_circle),
+          )
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+              ),
+              child: Text(
+                'Menú',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.camera),
+              title: Text('Fotos'),
+            ),
+            ListTile(
+              leading: Icon(Icons.smart_toy),
+              title: Text('Chat Bot'),
+            ),
+            ListTile(
+              leading: Icon(Icons.coronavirus),
+              title: Text('Covid Info'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
