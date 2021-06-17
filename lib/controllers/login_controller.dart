db4873a2b9070317a9ff7f7b1bbafec3222f7ec5
Roy Palomino <roy100995@gmail.com>
2021-06-16 19:20:59 -0500

    feat: added form validation

--- lib/controllers/login_controller.dart
+++ lib/controllers/login_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:get/state_manager.dart';
 import 'package:sgd_client_mobile/api/auth.dart';
 import 'package:sgd_client_mobile/models/auth/user.dart';
@@ -6,6 +7,10 @@ import 'package:get/route_manager.dart';
 import 'package:sgd_client_mobile/controllers/global_controller.dart';
 
 class LoginController extends GetxController {
+  GlobalKey<FormState> _formKey = GlobalKey();
+
+  GlobalKey<FormState> get formKey => _formKey;
+
   String _email = '';
   String _password = '';
 
@@ -19,21 +24,44 @@ class LoginController extends GetxController {
     this._password = password;
   }
 
-  onLogin() async {
-    isLoading = true;
-    update();
+  dynamic validateEmail(String email) {
+    if (email.trim().length < 1) {
+      return ("Este campo es obligatorio");
+    } else if (email.trim().length > 50) {
+      return ("No se debe exeder más de 50 caracteres");
+    }
+    return null;
+  }
 
-    LoggedUser? loggedUser =
-        await Auth.instance.login(this._email, this._password);
-    if (loggedUser != null) {
-      isLoading = false;
-      GlobalController().setKeys(loggedUser.access, loggedUser.refresh);
-      Future.delayed(Duration(seconds: 1), () {
-        Get.off(HomePage());
-      });
-    } else {
-      isLoading = false;
+  String? validatePassword(String password) {
+    if (password.trim().length < 1) {
+      return ("Este campo es obligatorio");
+    } else if (password.trim().length < 5) {
+      return ('La contraseña debe tener más de 5 caracteres');
+    } else if (password.trim().length > 30) {
+      return ('La contraseña no debe tener más de 30 caracteres');
+    }
+    return null;
+  }
+
+  onLogin() async {
+    bool isOk = _formKey.currentState!.validate();
+    if (isOk) {
+      isLoading = true;
       update();
+
+      LoggedUser? loggedUser =
+          await Auth.instance.login(this._email, this._password);
+      if (loggedUser != null) {
+        isLoading = false;
+        GlobalController().setKeys(loggedUser.access, loggedUser.refresh);
+        Future.delayed(Duration(seconds: 1), () {
+          Get.off(HomePage());
+        });
+      } else {
+        isLoading = false;
+        update();
+      }
     }
   }
 }
--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -29,88 +29,100 @@ class LoginPage extends StatelessWidget {
                   SizedBox(
                     height: 30,
                   ),
-                  Column(
-                    children: [
-                      Text(
-                        'Inicia Sesión',
-                        style: TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                          color: Theme.of(context).primaryColor,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(left: 15, right: 30, top: 20),
-                        child: TextFormField(
-                          onChanged: _.onChangeEmail,
-                          decoration: const InputDecoration(
-                            icon: Icon(Icons.email),
-                            hintText: 'Correo Electrónico',
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(left: 15, right: 30, top: 20),
-                        child: TextFormField(
-                          onChanged: _.onChangePass,
-                          obscureText: true,
-                          decoration: const InputDecoration(
-                            icon: Icon(Icons.lock),
-                            hintText: 'Contraseña',
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(left: 30, right: 30, top: 50),
-                        child: GestureDetector(
-                          onTap: _.onLogin,
-                          child: Container(
-                            child: Center(
-                              child: _.isLoading
-                                  ? CircularProgressIndicator()
-                                  : Text(
-                                      'INGRESAR',
-                                      style: TextStyle(
-                                        fontSize: 20,
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.w600,
-                                      ),
-                                    )
-                            ),
-                            color: Theme.of(context).primaryColor,
-                            width: double.infinity,
-                            height: 50.0,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 20),
-                        child: Text(
-                          '¿No estás registrado?',
+                  Form(
+                    key: _.formKey,
+                    child: Column(
+                      children: [
+                        Text(
+                          'Inicia Sesión',
                           style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w500,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            color: Theme.of(context).primaryColor,
                           ),
                         ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(),
-                        child: TextButton(
-                          onPressed: () {
-                            Get.to(RegisterPage(), transition: Transition.rightToLeft);
-                          },
-                          child: Text(
-                            'Crea una cuenta',
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.w600,
-                              decoration: TextDecoration.underline,
-                              color: Theme.of(context).primaryColorDark,
+                        Padding(
+                          padding:
+                              EdgeInsets.only(left: 15, right: 30, top: 20),
+                          child: TextFormField(
+                            onChanged: _.onChangeEmail,
+                            decoration: const InputDecoration(
+                              icon: Icon(Icons.email),
+                              hintText: 'Correo Electrónico',
+                            ),
+                            validator: (email) {
+                              return _.validateEmail(email as String);
+                            },
+                          ),
+                        ),
+                        Padding(
+                          padding:
+                              EdgeInsets.only(left: 15, right: 30, top: 20),
+                          child: TextFormField(
+                            onChanged: _.onChangePass,
+                            obscureText: true,
+                            decoration: const InputDecoration(
+                              icon: Icon(Icons.lock),
+                              hintText: 'Contraseña',
+                            ),
+                            validator: (password){
+                              return _.validatePassword(password as String);
+                            },
+                          ),
+                        ),
+                        Padding(
+                          padding:
+                              EdgeInsets.only(left: 30, right: 30, top: 50),
+                          child: GestureDetector(
+                            onTap: _.onLogin,
+                            child: Container(
+                              child: Center(
+                                  child: _.isLoading
+                                      ? CircularProgressIndicator()
+                                      : Text(
+                                          'INGRESAR',
+                                          style: TextStyle(
+                                            fontSize: 20,
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w600,
+                                          ),
+                                        )),
+                              color: Theme.of(context).primaryColor,
+                              width: double.infinity,
+                              height: 50.0,
                             ),
                           ),
                         ),
-                      ),
-                    ],
+                        Padding(
+                          padding: EdgeInsets.only(top: 20),
+                          child: Text(
+                            '¿No estás registrado?',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(),
+                          child: TextButton(
+                            onPressed: () {
+                              Get.to(RegisterPage(),
+                                  transition: Transition.rightToLeft);
+                            },
+                            child: Text(
+                              'Crea una cuenta',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600,
+                                decoration: TextDecoration.underline,
+                                color: Theme.of(context).primaryColorDark,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
